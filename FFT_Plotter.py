90265af366999fd73afdc8054face953846669e1
XERN0N <sigurdmjn@gmail.com>
2025-04-07 08:10:38 +0200

    various small changes in fft-plotter

--- FFT_Plotter.py
+++ FFT_Plotter.py
@@ -124,7 +124,7 @@ def process_file(file_path):
     Velocity_signal = []
     fft_results = []
 
-    sos = sig.butter(2, 0.5, 'highpass', output='sos')
+    sos = sig.butter(2, 0.01, 'highpass', output='sos')
     for i in range(4):
         data.iloc[:, i] = sig.sosfilt(sos, data.iloc[:, i])
 
@@ -171,51 +171,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-
-
-
-'''
-for file in File_list:
-    file_path = os.path.join(Data_folder_path, file)
-    print(f"Processing file: {file_path}")
-
-    try:
-        # Read file with no header (assuming no column names)
-        Data = pd.read_csv(file_path, dtype=float, delimiter=",")
-        print(Data.head()) #Preview data
-
-        # Extract first three rows (Description part)
-        Description = Data.iloc[:3].fillna('').astype(str)
-
-    except Exception as e:
-        print(f"Error processing {file}: {e}")
-
-Time = Data[:, 0]
-Signal_length = Time.size
-Delta_T = Time[-1]/Signal_length                                            #taking avg delta T and rounds
-Signal = Data[:, 1]                                                                  #extract signaly
-fft_signal = fft(Signal)                                                           #Fourier coefficients
-fft_signal_mag = np.abs(fft_signal)
-fft_freq_signal = fftfreq(Signal_length, Delta_T)[:Signal_length//2]
-num_max_values = 4
-largest_indices = np.argpartition(fft_signal_mag[:Signal_length//2], -num_max_values)[-num_max_values:]
-largest_values = fft_signal_mag[largest_indices]
-max = 2.0/Signal_length * largest_values
-max_loc = fft_freq_signal[largest_indices] * 2 * np.pi
-
-peakIdx = np.argmax(max)
-peakX = max_loc[peakIdx]
-peakY = max[peakIdx]
-print(peakX)
-print(peakY)
-
-plt.plot(2*np.pi*fft_freq_signal, 2.0/Signal_length * fft_signal_mag[:Signal_length//2]) #Single-sided fourier
-plt.plot(max_loc, max,'o')
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Amplitude')
-#plt.xlim(0, 1/Delta_T*1/2)
-plt.xlim(0, 350)
-plt.title('FFT of the Signal')
-plt.show()'
-print(f"Error processing {file}: {e}")'''
