4a8f4d0c736c76264b525bbe6f870659b6f38474
Oskar-AU <201905773@post.au.dk>
2025-05-17 14:53:36 +0200

    Updated original model to have vertices at sensor and hammer locations

--- FreeVibrationSimulation.py
+++ FreeVibrationSimulation.py
@@ -1,21 +1,21 @@
-from OriginalModel import generate_original_model
+from OriginalModel import *
 from SystemSolvers import *
 import matplotlib.pyplot as plt
 import pandas as pd
 
 model = generate_original_model()
 
-with model.added_forces({1: lambda t: [0, 125, 0, 0, 0, 0]}):
+with model.added_forces({8: lambda t: [0, 130, 0, 0, 0, 0]}):
     initial_condition_solver = Static(model).solve()
 
 end_time = 15.5
-time_increment = 0.00001
+time_increment = 0.0001
 solution = Newmark(model, initial_condition_solver, end_time, time_increment).solve().solution
 
 data = pd.read_csv("Sorted timeseries/Free/1D/Calibrated/strong_axis_data/timeseries_gr3_FirstStiff.txt")
 
 plt.plot(data['Time (us)'], data['Sensor 4 - X'], label='Experiment')
-plt.plot(solution.time, solution.accelerations[:, 73], label='Model')
+plt.plot(solution.time, solution.accelerations[:, Vertex_DOFs.output_primary_DOFs.value[-1]], label='Model')
 plt.xlabel('Time [$s$]')
 plt.ylabel('Accelerations [$m/s^2$]')
 plt.legend()
--- OriginalModel.py
+++ OriginalModel.py
@@ -1,6 +1,13 @@
 from SystemModels import Beam_Lattice
+from enum import Enum
 
-def generate_original_model(number_of_elements: int = 15) -> Beam_Lattice:
+class Vertex_DOFs(Enum):
+    output_primary_DOFs = (1, 7, 19, 31)
+    output_secondary_DOFs = (0, 6, 18, 30)
+    input_primary_DOFs = (13, 25, 37)
+    input_secondary_DOFs = (12, 24, 36)
+
+def generate_original_model(number_of_elements: int = 2, E_modulus: float = 2.1e11, density: float = 7850) -> Beam_Lattice:
     """
     Generates the original model "before" running the SSI.
     Sources:
@@ -9,7 +16,11 @@ def generate_original_model(number_of_elements: int = 15) -> Beam_Lattice:
     Parameters
     ----------
     number_of_elements : int, optional
-        The number of elements in the vertical beam.
+        The number of elements between each vertex where a vertex is placed at each input and output location.
+    E_modulus : float, optional
+        The modulus of elasticity for the beam [Pa]. Default 210 GPa.
+    density : float, optional
+        The density of the beam material [kg/m^3]. Default 7850 kg/m^3.
 
     Returns
     -------
@@ -18,24 +29,59 @@ def generate_original_model(number_of_elements: int = 15) -> Beam_Lattice:
     """
     model = Beam_Lattice()
 
+    vertex_heights = (0.34, 0.68, 0.81, 1.02, 1.24, 1.36, 1.60, 1.715)
+
+    # Start beam.
     model.add_beam_edge(
         number_of_elements=number_of_elements, 
-        E_modulus=2.1e11, 
+        E_modulus=E_modulus, 
         shear_modulus=7.9e10,
         primary_moment_of_area=1.94e-8,
         secondary_moment_of_area=1.02e-8,
         torsional_constant=2.29e-8,
-        density=7850, 
+        density=density, 
         cross_sectional_area=1.74e-4, 
-        coordinates=[[0, 0, 0], [0, 0, 1.7]],
+        coordinates=((0, 0, 0), (0, 0, vertex_heights[0])),
+        edge_polar_rotation=0
+    )
+
+    # Bulk beams.
+    for i, vertex_height in enumerate(vertex_heights[1:-1]):
+        model.add_beam_edge(
+            number_of_elements=number_of_elements, 
+            E_modulus=E_modulus, 
+            shear_modulus=7.9e10,
+            primary_moment_of_area=1.94e-8,
+            secondary_moment_of_area=1.02e-8,
+            torsional_constant=2.29e-8,
+            density=density, 
+            cross_sectional_area=1.74e-4, 
+            coordinates=(0, 0, vertex_height),
+            vertex_IDs=i+1,
+            edge_polar_rotation=0
+        )
+
+    # End beam.
+    model.add_beam_edge(
+        number_of_elements=number_of_elements, 
+        E_modulus=E_modulus, 
+        shear_modulus=7.9e10,
+        primary_moment_of_area=1.94e-8,
+        secondary_moment_of_area=1.02e-8,
+        torsional_constant=2.29e-8,
+        density=density, 
+        cross_sectional_area=1.74e-4, 
+        coordinates=(0, 0, vertex_heights[-1]),
+        vertex_IDs=7,
         edge_polar_rotation=0,
         point_mass=1.31,
         point_mass_moment_of_inertias=(0,0,0),
         point_mass_location='end_vertex'
     )
+
     model.fix_vertices((0,))
-    model.set_damping_ratio(0.0015)
-    
+    model.set_damping_ratio(0.0013)
+
     return model
 
 """ Determination of the default number of elements for the generate_original_model function. """
@@ -43,26 +89,25 @@ if __name__ == "__main__":
     import numpy as np
     import matplotlib.pyplot as plt
 
-    element_multipliers = np.arange(1, 6)
+    number_of_elements = np.arange(1, 8)
 
     norms = []
-    for element_multiplier in element_multipliers:
-        number_of_elements = 5 * element_multiplier
+    DOFs = []
+    for number_of_element in number_of_elements:
+        model = generate_original_model(number_of_element)
 
-        model = generate_original_model(number_of_elements)
-
-        output_DOFs = np.arange(1, 5) * element_multiplier * 6
-
-        state_matrix, _, output_matrix, _, = model.get_state_space_matrices('receptence', output_DOFs=output_DOFs)
+        state_matrix, _, output_matrix, _, = model.get_state_space_matrices('receptence', output_DOFs=np.concatenate((Vertex_DOFs.output_primary_DOFs.value, 
+                                                                                                                      Vertex_DOFs.output_secondary_DOFs.value)))
 
         eigen_values, eigen_vectors = np.linalg.eig(state_matrix)
 
         mode_shapes = output_matrix @ eigen_vectors
+        
+        norms.append(np.linalg.matrix_norm(mode_shapes[:, :5]))
+        DOFs.append(model.system_DOF - len(model.fixed_DOFs))
 
-        norms.append(np.linalg.matrix_norm(mode_shapes[:, :30]))
-
-    plt.plot(element_multipliers * 6 * 5, norms, marker='o')
-    plt.title("Matrix norm of the first 30 modes as a function of DOF")
+    plt.plot(DOFs, norms, marker='o')
+    plt.title("Matrix norm of the first 5 modes as a function of DOF")
     plt.xlabel("DOF")
     plt.ylabel("$\\|C_c\\Psi_c\\|$")
     plt.grid()
