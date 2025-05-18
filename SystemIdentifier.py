f00ef660d75927e74d89188064822ac801bdefcb
Oskar-AU <201905773@post.au.dk>
2025-05-18 17:47:03 +0200

    Opdated code in modalexpander and modelFRF to the new original model and with more documentation

--- SystemIdentifier.py
+++ SystemIdentifier.py
@@ -4,7 +4,8 @@ from scipy.linalg import svd,logm
 import matplotlib.pyplot as plt
 from SystemModels import Beam_Lattice
 from scipy.signal import find_peaks
-
+from OriginalModel import *
+from typing import Literal
 
 np.set_printoptions(threshold=np.inf)
 np.set_printoptions(linewidth=10000, suppress=True, precision=3)
@@ -144,64 +145,55 @@ data5 = np.loadtxt('Sorted timeseries/Free/1D/Calibrated/Torsional_axis_data/X/t
 #print(dr)
 #print(phi)
 
-def modalexpander(data, estimated_DOF, data_direction, number_of_elements):
-    if number_of_elements % 5 != 0:
-        raise ValueError("The number_of_elements must be divisible by 5.")
-    system = Beam_Lattice()
-    e = number_of_elements//5
-    primary_moment_of_area = 1.94e-8
-    secondary_moment_of_area = 1.02e-8
-    torsional_constant = 2.29e-8
-    cross_sectional_area = 1.74e-4
+def modalexpander(acceleration_data_path: str, estimated_accelerometer_number: int, axis: Literal['primary', 'secondary']) -> None:
+    """
+    Estimates the acceleration at a specified accelerometer location and plots the estimation vs actual acceleration over time.
 
-    system.add_beam_edge(
-        number_of_elements=number_of_elements,
-        E_modulus=2.1e11,
-        shear_modulus=7.9e10,
-        primary_moment_of_area=primary_moment_of_area,
-        secondary_moment_of_area=secondary_moment_of_area,
-        torsional_constant=torsional_constant,
-        density=7850,
-        cross_sectional_area=cross_sectional_area,
-        coordinates=((0, 0, 0), (0, 0, 1.7)),
-        edge_polar_rotation=0,
-        point_mass=1.31,
-        point_mass_moment_of_inertias=(0.0004384,0.0011301,0.000861),
-        point_mass_location='end_vertex'
-    )
+    Parameters
+    ----------
+    acceleration_data_path : str
+        The relative path to the acceleration data from this git repo.
+    estimated_accelerometer_number : int
+        The accelerameter number to estimate (from 0 to 3) where 0 is the bottom-most.
+    axis : str
+        The axis to to modal expansion on. Can be either 'primary' or 'secondary'.
+    """
+    acceleration_data = np.loadtxt(acceleration_data_path, delimiter=',', skiprows=1, usecols=(3, 4, 5, 6))
 
-    system.fix_vertices((0,))
+    system = generate_original_model()
     eigfreq, eigvec, damped = system.get_modal_param()
    
     print(eigvec[:, :15])
     print(eigfreq[:15])
     print(eigvec.shape)
-    if data_direction == 'x':
-        mu1 = [6*e,12*e,18*e,24*e]
-        col_pick = [2,4,6]
-        #start = 0
-        #end = 3
-    if data_direction == 'y':
-        mu1 = [7*e,13*e,19*e,25*e]
-        col_pick = [3,5,7]
-    mu2 = mu1[estimated_DOF]
+    
+    match axis:
+        case 'primary':
+            mu1 = list(Vertex_DOFs.output_primary_DOFs.value)
+            col_pick = [1,3,5]
+        case 'secondary':
+            mu1 = list(Vertex_DOFs.output_secondary_DOFs.value)
+            col_pick = [2,4,6]
+        case _:
+            raise ValueError(f"'axis' expected either 'primary' or 'secondary' but recieved '{axis}'.")
+    
+    mu2 = mu1[estimated_accelerometer_number]
     mu1.remove(mu2)
+    print(mu1)
     
     phi_mu1 = eigvec[mu1].T[col_pick].T
     phi_mu2 = eigvec[mu2].T[col_pick].T
     print(phi_mu1)
     print(phi_mu2)
-    measured_data = data[:, [i for i in range(4) if i != estimated_DOF]].T
+    measured_data = acceleration_data[:, [i for i in range(4) if i != estimated_accelerometer_number]].T
     least_squares = np.linalg.pinv(phi_mu1) @ measured_data
     
     estimated_data = phi_mu2 @ least_squares
     
-    scaling_factor = np.linalg.lstsq(estimated_data.reshape(-1, 1), data[:, estimated_DOF].reshape(-1, 1), rcond=None)[0]
+    scaling_factor = np.linalg.lstsq(estimated_data.reshape(-1, 1), acceleration_data[:, estimated_accelerometer_number].reshape(-1, 1), rcond=None)[0]
     estimated_data_scaled = estimated_data * scaling_factor[0]  # scaling_factor is a 1-element array
 
-
-
-    plt.plot(data[:, estimated_DOF], '--r', label='Measured')
+    plt.plot(acceleration_data[:, estimated_accelerometer_number], '--r', label='Measured')
     plt.plot(estimated_data_scaled, 'k', label='Estimated', alpha=0.5)
     plt.xlabel("Time step")
     plt.ylabel("Acceleration")
@@ -209,34 +201,32 @@ def modalexpander(data, estimated_DOF, data_direction, number_of_elements):
     plt.title(f"Acceleration at DOF {mu2}")
     plt.show() 
 
-def modelFRF(number_of_elements,accelerometer_location, axis):
-  
+def modelFRF(accelerometer_location: int, hammer_location: int, axis: Literal['primary', 'secondary'], FRF_kinematic: Literal['receptence', 'mobility', 'accelerance']) -> None:
+    """
+    Plots the model FRF.
 
-    #The top most accelerometer is 1, the bottom most is 4
+    Parameters
+    ----------
+    accelerometer_location : int
+        The accelorameter number from 0 to 3 where 0 is the buttom-most accelerometer.
+    hammer_location : int
+        The hammer location number from 0 to 2 where 0 is the buttom-most hammer location.
+    axis : str
+        Which axis to get the FRF for. Can be either 'primary' or 'secondary'.
+    """
+    match axis:
+        case 'primary':
+            output_DOF = Vertex_DOFs.output_primary_DOFs.value[accelerometer_location]
+            input_DOF = Vertex_DOFs.input_primary_DOFs.value[hammer_location]
+        case 'secondary':
+            output_DOF = Vertex_DOFs.output_secondary_DOFs.value[accelerometer_location]
+            input_DOF = Vertex_DOFs.input_secondary_DOFs.value[hammer_location]
+        case _:
+            raise ValueError(f"'axis' expected either 'primary' or 'secondary' but recieved '{axis}'.")
 
-    
-    f = accelerometer_location*round(0.34*number_of_elements/1.7)
-    k = (number_of_elements+1-f)*6+axis
-    system = Beam_Lattice()
-    
-    system.add_beam_edge(
-        number_of_elements=number_of_elements, 
-        E_modulus=2.1*10**11, 
-        shear_modulus=7.9*10**10,
-        primary_moment_of_area=2.157*10**-8,
-        secondary_moment_of_area=1.113*10**-8,
-        torsional_constant=3.7*10**-8,
-        density=7850, 
-        cross_sectional_area=1.737*10**-4, 
-        coordinates=[[0, 0, 0], [0, 0, 1.7]],
-        edge_polar_rotation=0,
-        point_mass=1.31,
-        point_mass_moment_of_inertias=(0.0004384,0.0011301,0.000861),
-        point_mass_location='end_vertex'
-    )
-    system.fix_vertices((0,))
-    eigfreq, eigvec,_ = system.get_modal_param()
-    mass_matrix,_,_ = system.get_system_level_matrices()
+    model = generate_original_model()
+    eigfreq, eigvec,_ = model.get_modal_param()
+    mass_matrix,_,_ = model.get_system_level_matrices()
     freq = np.linspace(0, 300, 1000)  # Frequency range for FRF calculation
     omega = 2 * np.pi * freq  # convert to rad/s
     omega_n = 2 * np.pi * eigfreq  # convert to rad/s
@@ -246,14 +236,22 @@ def modelFRF(number_of_elements,accelerometer_location, axis):
     for j in range(n_modes):
         phi_j = eigvec[:, j].reshape(-1, 1)
         m_j = (phi_j.T @ mass_matrix @ phi_j).item()
-        num = phi_j[k, 0] * phi_j[k, 0]
+        num = phi_j[output_DOF, 0] * phi_j[input_DOF, 0]
         denom = omega_n[j]**2 - omega**2 + 2j * 0.005 * omega_n[j] * omega
         h_kl += num / (m_j * denom)
 
-    Z_kl = -omega**2 * h_kl
+    match FRF_kinematic:
+        case 'receptence':
+            Z_kl = h_kl
+        case 'mobility':
+            Z_kl = omega * h_kl
+        case 'accelerance':
+            Z_kl = -omega**2 * h_kl
+        case _:
+            raise ValueError(f"'FRF_kinematic' recieved an unexpected input. Expected either 'receptence', 'mobility', 'accelerance' but recieved '{FRF_kinematic}'.")
+
     frf_mag = np.abs(Z_kl)  # Magnitude of the FRF
     peaks, _ = find_peaks(frf_mag, height=0)
-
     
     plt.figure(figsize=(8, 4))
     plt.plot(freq, frf_mag, color='orange')
@@ -264,96 +262,96 @@ def modelFRF(number_of_elements,accelerometer_location, axis):
         plt.plot(f_peak, m_peak, 'ro', markersize=6)  # red circle
         plt.text(f_peak, m_peak * 1.05, f'{f_peak:.1f} Hz', ha='center', va='bottom', fontsize=9)
 
-    plt.title(f'|Z_{{{k+1}{k+1}}}(ω)| Inertance FRF')
+    plt.title(f'{FRF_kinematic} FRF with input DOF = {input_DOF} and output DOF = {output_DOF}')
     plt.xlabel('Frequency [Hz]')
-    plt.ylabel('Magnitude [m/s² per N]')
+    plt.ylabel('$|h|$')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    #fre1, bla, tis = system_identifier(20,50,data4,0.001079)
+    #[    4.218     5.817    36.564    50.426   112.377   154.981   230.919   318.463   392.557   410.525   503.7     541.382   597.501 ]
+    model_f = [4.217,     5.816,    36.424,    50.279,   111.192,   153.74,    216.241,   225.925,   313.277,   377.456,   503.7,     525.906,   559.812]
+
+    #modalexpander(data2,3,'x',15)
+    #modelFRF(40,1,1)
+    #fre1, _, _ = system_identifier(30, 50, data, 0.001079)
+
+    #print(fre1)
+    #print(bla)
+    #print(tis)
+    #Kig i lecture 10, for at lave FRF baseret på måle data i forced vibration
+    #model_order_selector(10,30,50,data6,0.001079)
+
+
+    datasets = [data1, data2, data3, data4, data5]
+    dataset_labels = [f"data{i+1}" for i in range(len(datasets))]
+
+    # Parameters for system_identifier
+
+    dt = 0.001079
+
+    # Storage for eigenfrequencies
+    all_frequencies = []
+
+    # Loop through datasets
+    for i, data in enumerate(datasets):
+        freqs, _, _ = system_identifier(30, 50, data, dt)
+        all_frequencies.append(freqs)
+
+    # Plotting
+    plt.figure(figsize=(10, 6))
+    for idx, freqs in enumerate(all_frequencies):
+        y = np.full_like(freqs, idx + 1)  # assign y-position based on dataset number
+        plt.plot(freqs, y, 'o', label=dataset_labels[idx])
+    for f in model_f:
+        plt.axvline(f, color='red', linestyle='--', linewidth=1, alpha=0.7)
+    plt.yticks(ticks=range(1, 6), labels=['weak-axis','weak-axis-hammer','strong-axis','strong-axis-hammer','twisting'])
+    plt.xlabel("Eigen-frequency [Hz]")
+    plt.ylabel("Dataset")
+    plt.title("Eigenfrequencies per dataset")
     plt.grid(True)
     plt.tight_layout()
     plt.show()
 
 
-#fre1, bla, tis = system_identifier(20,50,data4,0.001079)
-#[    4.218     5.817    36.564    50.426   112.377   154.981   230.919   318.463   392.557   410.525   503.7     541.382   597.501 ]
-model_f = [4.217,     5.816,    36.424,    50.279,   111.192,   153.74,    216.241,   225.925,   313.277,   377.456,   503.7,     525.906,   559.812]
+    '''
+    a = 9
+    N = A.shape[0]
+    b = N-2*a+1
+    m = A.shape[1]
+    print(b)
 
-#modalexpander(data2,3,'x',15)
-#modelFRF(40,1,1)
-#fre1, _, _ = system_identifier(30, 50, data, 0.001079)
+    print(2*a)
 
-#print(fre1)
-#print(bla)
-#print(tis)
-#Kig i lecture 10, for at lave FRF baseret på måle data i forced vibration
-#model_order_selector(10,30,50,data6,0.001079)
+    result = np.hstack([A[i, :].reshape(-1, 1) for i in range(b-1)])
+    result2 = np.vstack((result, result))
+    Yp = deque(maxlen=b)
+    Yf = deque(maxlen=b)
+    for i in range(b-1):
+        Yp.append(A[i, :].reshape(-1, 1))
+        Yf.append(A[a+i, :].reshape(-1, 1))
 
+    stacked_Yp = np.hstack(Yp)
+    stacked_Yf = np.hstack(Yf)
 
-datasets = [data1, data2, data3, data4, data5]
-dataset_labels = [f"data{i+1}" for i in range(len(datasets))]
+    print(stacked_Yp)
+    print(stacked_Yf)    
+    for i in range(a-1):
+        Yp.append(A[b+i, :].reshape(-1, 1))
+        Yp.popleft()
+        Yf.append(A[b+a+i, :].reshape(-1, 1))
+        Yf.popleft()
+        stacked_Yp = np.vstack((stacked_Yp,np.hstack(Yp)))
+        stacked_Yf = np.vstack((stacked_Yf,np.hstack(Yf)))          
 
-# Parameters for system_identifier
+    result3 = np.vstack((stacked_Yp, stacked_Yf))
 
-dt = 0.001079
-
-# Storage for eigenfrequencies
-all_frequencies = []
-
-# Loop through datasets
-for i, data in enumerate(datasets):
-    freqs, _, _ = system_identifier(30, 50, data, dt)
-    all_frequencies.append(freqs)
-
-# Plotting
-plt.figure(figsize=(10, 6))
-for idx, freqs in enumerate(all_frequencies):
-    y = np.full_like(freqs, idx + 1)  # assign y-position based on dataset number
-    plt.plot(freqs, y, 'o', label=dataset_labels[idx])
-for f in model_f:
-    plt.axvline(f, color='red', linestyle='--', linewidth=1, alpha=0.7)
-plt.yticks(ticks=range(1, 6), labels=['weak-axis','weak-axis-hammer','strong-axis','strong-axis-hammer','twisting'])
-plt.xlabel("Eigen-frequency [Hz]")
-plt.ylabel("Dataset")
-plt.title("Eigenfrequencies per dataset")
-plt.grid(True)
-plt.tight_layout()
-plt.show()
-
-
-'''
-a = 9
-N = A.shape[0]
-b = N-2*a+1
-m = A.shape[1]
-print(b)
-
-print(2*a)
-
-result = np.hstack([A[i, :].reshape(-1, 1) for i in range(b-1)])
-result2 = np.vstack((result, result))
-Yp = deque(maxlen=b)
-Yf = deque(maxlen=b)
-for i in range(b-1):
-    Yp.append(A[i, :].reshape(-1, 1))
-    Yf.append(A[a+i, :].reshape(-1, 1))
-
-stacked_Yp = np.hstack(Yp)
-stacked_Yf = np.hstack(Yf)
-
-print(stacked_Yp)
-print(stacked_Yf)    
-for i in range(a-1):
-    Yp.append(A[b+i, :].reshape(-1, 1))
-    Yp.popleft()
-    Yf.append(A[b+a+i, :].reshape(-1, 1))
-    Yf.popleft()
-    stacked_Yp = np.vstack((stacked_Yp,np.hstack(Yp)))
-    stacked_Yf = np.vstack((stacked_Yf,np.hstack(Yf)))          
-
-result3 = np.vstack((stacked_Yp, stacked_Yf))
-
-print(result3)
-'''
+    print(result3)
+    '''
 
 
 
 
-   
+    
