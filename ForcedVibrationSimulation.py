e107cf3a2987257531f35dc4c5bbf44a865e3f33
Oskar-AU <201905773@post.au.dk>
2025-05-19 09:57:13 +0200

    Plots shaped displacements

--- ForcedVibrationSimulation.py
+++ ForcedVibrationSimulation.py
@@ -3,18 +3,22 @@ from SystemSolvers import *
 import matplotlib.pyplot as plt
 import pandas as pd
 from Forces import continous_force_from_array
+import matplotlib.animation as animation
 
-model_primary = generate_original_model()
+model = generate_original_model()
 
-# initial_condition_solver = Static(model).solve()
+initial_condition_solver = Static(model).solve()
 
-hammer_data = pd.read_csv("Sorted timeseries/Forced/2D/Hammer/Calibrated/X/timeseries_gr3_2D_middle_2025-03-27-10-22-24.csv.csv")
+# hammer_data = pd.read_csv("Sorted timeseries/Forced/1D/Hammer/Calibrated/X/timeseries_gr3_1D_x_middle2025-03-27-10-54-23.csv.csv", skiprows=range(1, 9750), nrows=25000)
 
-# model.add_forces({5: continous_force_from_array(np.column_stack((hammer_data['Hammer force [N]'], np.zeros((len(hammer_data['Hammer force [N]']), 5)))).T, hammer_data['Time (s)'])})
+hammer_data = pd.read_csv("Sorted timeseries/Forced/1D/Hammer/Calibrated/X/timeseries_gr3_1D_x_middle_1hit2025-03-27-10-53-34.csv.csv", skiprows=range(1, 5500), nrows=4500)
+
+model.add_forces({5: continous_force_from_array(np.column_stack((hammer_data['Hammer force [N]'], np.zeros((len(hammer_data['Hammer force [N]']), 5)))).T, hammer_data['Time (s)'])})
 
 
-# end_time = 15.5
-# time_increment = 0.001
+end_time = 15.5
+time_increment = 0.001
+scaling_factor = 10000
 
 """ model_forces = []
 timesteps = np.arange(0, end_time, time_increment)
@@ -29,16 +33,39 @@ plt.legend()
 plt.show()
 """
     
-# solution = Newmark(model, initial_condition_solver, end_time, time_increment).solve().solution
+newmark_solver = Newmark(model, initial_condition_solver, end_time, time_increment).solve()
 
-accelerometer_data = pd.read_csv("Sorted timeseries/Forced/2D/Accelerometer/Calibrated/timeseries_serial_output_2_gr3_2D_middle.txt")
+solution = newmark_solver.solution
 
-plt.plot(accelerometer_data['Time (us)'], accelerometer_data['Sensor 4 - X'], label='accelerometer')
-plt.plot(hammer_data['Time (s)'], hammer_data['Hammer force [N]'], label='Hammer')
+# plt.plot(accelerometer_data['Time (us)'], accelerometer_data['Sensor 4 - X'], label='accelerometer')
+# plt.plot(hammer_data['Time (s)'], hammer_data['Hammer force [N]'], label='Hammer')
 
-# plt.plot(accelerometer_data['Time (us)'], accelerometer_data['Sensor 4 - X'], label='Experiment')
-# plt.plot(solution.time, solution.accelerations[:, Vertex_DOFs.output_secondary_DOFs.value[-1]], label='Model')
+displaced_shape_points = newmark_solver.get_displaced_shape_position(scaling_factor)
+
+plot_lines = list()
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+for displaced_shape_point in displaced_shape_points:
+    plot_lines.append(ax.plot(displaced_shape_point[0, :, 0], displaced_shape_point[0, :, 1], displaced_shape_point[0, :, 2], linewidth=2.0))
+
+def update(frame):
+    for i, lines in enumerate(plot_lines):
+        for line in lines:
+            line.set_data_3d(*displaced_shape_points[i][frame, :].T)
+    ax.set_title(f"t = {frame*time_increment:.3f} s")
+    return plot_lines
+
+ani = animation.FuncAnimation(fig=fig, func=update, frames=int(end_time/time_increment*10), interval=int(time_increment)*10)
+
+ax.axis('equal')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+ax.grid()
+plt.show()
+
+""" plt.plot(solution.time, solution.accelerations[:, Vertex_DOFs.output_secondary_DOFs.value[-1]], label='Model')
 plt.xlabel('Time [$s$]')
 plt.ylabel('Accelerations [$m/s^2$]')
 plt.legend()
-plt.show()
+plt.show() """
